fac15ff7f75de4d7c15839dc13278940ffbf34a4
cbattaile <cbattail@wellesley.edu>
2017-04-25 20:12:48 -0400

    master.java added

--- Master.java
+++ Master.java
@@ -22,6 +22,7 @@ public class Master implements IntMaster {
 	private HashMap<IntWorker, Boolean> workerStubs; 
 	private ArrayList<String> workerIPs;
 	private IntWorker ownWorkerStub;
+	private ArrayList<IntWorker> workerStubList; //arraylist of all non-self worker stubs
 	
 	private HashMap<String, IntMapTask> mapperTasks; // the actual mapper tasks
 	private HashMap<String, Boolean> mapperTasksDone; // whether a map is done
@@ -29,7 +30,7 @@ public class Master implements IntMaster {
 	private HashMap<String, Boolean> reducerTasksDone;
 
 	public Master(String myIP, String jid, String filename, ArrayList<String> workerIPs,
-			HashMap<IntWorker, Boolean> workerStubs, IntWorker ownWorkerStub) {
+		HashMap<IntWorker, Boolean> workerStubs, IntWorker ownWorkerStub) {
 		ownIP = myIP;
 		jobID = jid;
 		masterStub = null;
@@ -46,9 +47,8 @@ public class Master implements IntMaster {
 		mapperTasksDone = new HashMap<String, Boolean>();
 		reducerTasks = new HashMap<String, IntReduceTask>();
 		reducerTasksDone = new HashMap<String, Boolean>();
-		
-		// start Master stub
-
+		workerStubs.remove(ownWorkerStub);
+		workerStubList = new ArrayList<IntWorker>(workerStubs.keySet());
 
 	}
 	
@@ -57,11 +57,111 @@ public class Master implements IntMaster {
 	}
 	
 	public void start() {
+		workerStubs.remove(ownWorkerStub);
 		try {
 			this.writer = new PrintWriter(outputFile);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		try {
+			currentlyReading = true;
+			FileReader fr = new FileReader(inputFilename);
+			BufferedReader bufr = new BufferedReader(fr);
+			String line = bufr.readLine();
+			int count = 0;
+			int numWorkers = workerStubs.size()-1;
+			// 2. for each line, master starts task on one of the mapper nodes
+			// ^^use FCFS
+			while (line != null) {
+				System.out.println("processing: " + line);
+				// find a worker node and start a map task on it
+				String mapTaskName = "M" + jid + "_" + Integer.toString(count);
+				IntMapTask newMapTask = workerStubs.get(count % numWorkers).createMapTask(mapTaskName);
+				mapperTasks.put(mapperName, newMapTask);
+				mapperTasksDone.put(mapperName, false);
+				newMapTask.processInput(line, stub);
+				count++;
+				line = bufr.readLine();
+			}
+			bufr.close();
+			currentlyReading = false;
+
+			// master checks that the mappers are done
+			while (!mapperTasksDone.isEmpty()) {
+				System.out.println("mappers not done");
+			}
+			synchronized (reducerTasks) {
+				System.out.println("all mappers are done send terminate message to reducers");
+				for (String reducerName : reducerTasks.keySet()) {
+					IntReducer reducerStub = reducerTasks.get(reducerName);
+					try {
+						System.out.println("sending terminate to " + reducerName);
+						reducerStub.terminate();
+					} catch (Exception e) {
+						System.err.println("ReducerTask Exception - could not terminate" + e.toString());
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// mapper checks that the reducers are done
+			synchronized (reducerTasks) {
+				if (mapperTasksDone.isEmpty() && reducerTasksDone.isEmpty() && !currentlyReading) {
+					System.out.println("finished, closing and writing file");
+					writer.close();
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Master Exception - Cannot read file" + e.toString());
+			e.printStackTrace();
+		}
+
 	} 
+
+	public IntReduceTask[] getReducers(String[] keys) {
+		IntReduceTask[] matchingReducers = new IntReduceTask[keys.length + 1];
+		for (int i = 0; i < keys.length; i++) {
+			String reducerName = "R" + jid + "_" + keys[i];
+			if (reducerTasks.containsKey(reducerName)) {
+				matchingReducers[i] = reducerTasks.get(reducerName);
+			} else {
+				// create new reducerTask
+				IntWorker worker = workerStubList.get(reducerName.hashCode() % workerStubList.size());
+				IntReduceTask newReducerTask;
+				try {
+					newReduceTask = worker.createReduceTask(reducerName, this.stub);
+					reducerTasks.put(reducerName, newReduceTask);
+					reducerTasksDone.put(reducerName, false);
+					matchingReducers[i] = newReduceTask;
+				} catch (Exception e) {
+					System.err.println("Client exception(could not add reduceTask): " + e.toString());
+					e.printStackTrace();
+				}
+
+			}
+		}
+		return matchingReducers;
+	}
+
+	public void receiveOutput(String key, int value) {
+		try {
+			System.out.println("writing to file: " + key + ":" + value);
+			writer.println(key + ":" + value);
+			// writer.close();
+			synchronized (reducerTasksDone) {
+				reducerTasksDone.remove(key);
+			}
+		} catch (Exception e) {
+			System.err.println("Master Exception - Cannot write file" + e.toString());
+			e.printStackTrace();
+		}
+
+	}
+
+	public void markMapperDone(String mapperName) {
+		System.out.println("calling mark Master done, removing " + mapperName + " from " + mapperTasksDone.keySet());
+		mapperTasksDone.remove(mapperName);
+		System.out.println("calling mark Master done, removed " + mapperName + " from " + mapperTasksDone.keySet());
+	}
 
 }
